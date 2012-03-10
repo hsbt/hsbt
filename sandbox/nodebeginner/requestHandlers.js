2af6a74cc7390282c04218dabb6317d8b5f87dc2
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2012-03-10 19:20:41 +0900

    displayed postData

--- sandbox/nodebeginner/requestHandlers.js
+++ sandbox/nodebeginner/requestHandlers.js
@@ -1,19 +1,30 @@
-var exec = require('child_process').exec;
+var querystring = require("querystring");
 
-function start() {
+function start(response, postData) {
     console.log("Request handler 'start' was called.");
-    var content = "empty";
 
-    exec("ls -lah", function (error, stdout, stderr) {
-        content = stdout;
-    });
+    var body = "<html>" +
+	"<head>" +
+	'<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />' +
+	"</head>" +
+	"<body>" +
+	'<form action="/upload" method="post">' +
+	'<textarea name="text" rows="20" cols="60"></textarea>' +
+	'<input type="submit" value="Submit text" />' +
+	"</form>" +
+	"</body>" +
+	"</html>";
 
-    return content;
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(body);
+    response.end();
 }
 
-function upload() {
+function upload(response, postData) {
     console.log("Request handler 'upload' was called.");
-    return "Hello Upload";
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("You've sent: " + querystring.parse(postData).text);
+    response.end();
 }
 
 exports.start = start;
--- sandbox/nodebeginner/server.js
+++ sandbox/nodebeginner/server.js
@@ -3,13 +3,20 @@ var url = require("url");
 
 function start(route, handle) {
     function onRequest(request, response) {
+	var postData = "";
 	var pathname = url.parse(request.url).pathname;
 	console.log("Request for " + pathname + " received.");
 
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	var content = route(handle, pathname);
-	response.write(content);
-	response.end();
+	request.setEncoding("utf8");
+
+	request.addListener("data", function(postDataChunk) {
+          postData += postDataChunk;
+          console.log("Received POST data chunk '" + postDataChunk + "'.");
+        });
+
+	request.addListener("end", function() {
+          route(handle, pathname, response, postData);
+        });
     }
 
     http.createServer(onRequest).listen(8888);
