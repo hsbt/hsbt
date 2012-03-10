ad672ac028a3d2093216cd1bc95550f12904a7e3
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2012-03-10 18:27:19 +0900

    added requestHandler

--- sandbox/nodebeginner/server.js
+++ sandbox/nodebeginner/server.js
@@ -1,12 +1,12 @@
 var http = require("http");
 var url = require("url");
 
-function start(route) {
+function start(route, handle) {
     function onRequest(request, response) {
 	var pathname = url.parse(request.url).pathname;
 	console.log("Request for " + pathname + " received.");
 
-	route(pathname);
+	route(handle, pathname);
 
 	response.writeHead(200, {"Content-Type": "text/plain"});
 	response.write("Hello World");
