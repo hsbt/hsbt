47de3468c6834abc861799bf283cccac64324bff
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2012-03-10 18:43:31 +0900

    blocking effects

--- sandbox/nodebeginner/requestHandlers.js
+++ sandbox/nodebeginner/requestHandlers.js
@@ -1,5 +1,12 @@
 function start() {
     console.log("Request handler 'start' was called.");
+
+    function sleep(milliSeconds) {
+	var startTime = new Date().getTime();
+	while (new Date().getTime() < startTime + milliSeconds);
+    }
+
+    sleep(10000);
     return "Hello Start";
 }
 
