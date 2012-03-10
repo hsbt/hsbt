f83b3239195b0ed2e406f51cd51fd2992dc89c0d
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2012-03-10 21:10:39 +0900

    impl upload form

--- sandbox/nodebeginner/requestHandlers.js
+++ sandbox/nodebeginner/requestHandlers.js
@@ -1,6 +1,6 @@
-var querystring = require("querystring"), fs = require('fs');
+var querystring = require("querystring"), fs = require('fs'), formidable = require('formidable');
 
-function start(response, postData) {
+function start(response) {
     console.log("Request handler 'start' was called.");
 
     var body = "<html>" +
@@ -8,9 +8,9 @@ function start(response, postData) {
 	'<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />' +
 	"</head>" +
 	"<body>" +
-	'<form action="/upload" method="post">' +
-	'<textarea name="text" rows="20" cols="60"></textarea>' +
-	'<input type="submit" value="Submit text" />' +
+	'<form action="/upload" enctype="multipart/form-data" method="post">' +
+	'<input type="file" name="upload">' +
+	'<input type="submit" value="Upload file" />' +
 	"</form>" +
 	"</body>" +
 	"</html>";
@@ -20,14 +20,29 @@ function start(response, postData) {
     response.end();
 }
 
-function upload(response, postData) {
+function upload(response, request) {
     console.log("Request handler 'upload' was called.");
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("You've sent: " + querystring.parse(postData).text);
-    response.end();
+
+    var form = new formidable.IncomingForm();
+    console.log("about to parse");
+    form.parse(request, function(error, fields, files) {
+        console.log("parsing done");
+
+        fs.rename(files.upload.path, "/tmp/test.png", function(err) {
+          if (err) {
+            fs.unlink("/tmp/test.png");
+	    fs.rename(files.upload.path, "/tmp/test.png");
+	  }
+        });
+
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write("received image:<br />");
+        response.write("<img src='/show' />");
+        response.end();
+    });
 }
 
-function show(response, postData) {
+function show(response) {
     console.log("Request handler 'show' was called.");
     fs.readFile("/tmp/test.png", "binary", function(error, file) {
       if(error) {
--- sandbox/nodebeginner/router.js
+++ sandbox/nodebeginner/router.js
@@ -1,7 +1,7 @@
-function route(handle, pathname, response, postData) {
+function route(handle, pathname, response, request) {
     console.log("About to route a request for " + pathname);
     if(typeof handle[pathname] === 'function') {
-	return handle[pathname](response, postData);
+	return handle[pathname](response, request);
     } else {
 	console.log("No request handler found for " + pathname);
 	response.writeHead(404, {"Content-Type": "text/plain"});
