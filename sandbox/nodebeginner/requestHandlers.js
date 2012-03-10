c6be4553ff339727660898fde533677e29035b55
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2012-03-10 20:13:52 +0900

    displayed png image.

--- sandbox/nodebeginner/requestHandlers.js
+++ sandbox/nodebeginner/requestHandlers.js
@@ -1,4 +1,4 @@
-var querystring = require("querystring");
+var querystring = require("querystring"), fs = require('fs');
 
 function start(response, postData) {
     console.log("Request handler 'start' was called.");
@@ -27,5 +27,21 @@ function upload(response, postData) {
     response.end();
 }
 
+function show(response, postData) {
+    console.log("Request handler 'show' was called.");
+    fs.readFile("/tmp/test.png", "binary", function(error, file) {
+      if(error) {
+	  response.writeHead(500, {"Content-Type": "text/plain"});
+	  response.write(error + "\n");
+	  response.end();
+      } else {
+	  response.writeHead(500, {"Content-Type": "image/png"});
+	  response.write(file, "binary");
+	  response.end();
+      }
+    });
+}
+
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
