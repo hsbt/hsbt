b1f95902f5491d514d6a7b5adfe603efcc9ef87b
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2012-03-10 18:54:41 +0900

    replace exec process.

--- sandbox/nodebeginner/requestHandlers.js
+++ sandbox/nodebeginner/requestHandlers.js
@@ -1,13 +1,14 @@
+var exec = require('child_process').exec;
+
 function start() {
     console.log("Request handler 'start' was called.");
+    var content = "empty";
 
-    function sleep(milliSeconds) {
-	var startTime = new Date().getTime();
-	while (new Date().getTime() < startTime + milliSeconds);
-    }
+    exec("ls -lah", function (error, stdout, stderr) {
+        content = stdout;
+    });
 
-    sleep(10000);
-    return "Hello Start";
+    return content;
 }
 
 function upload() {
