1ea40f6cecbd87e1d5cc3fd719ef248ef8ea9972
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2012-03-10 17:47:37 +0900

    function

--- sandbox/nodebeginner/server.js
+++ sandbox/nodebeginner/server.js
@@ -1,7 +1,12 @@
 var http = require("http");
 
-http.createServer(function(request, response){
+function onRequest(request, response) {
+  console.log("Request received.");
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("Hello World");
   response.end();
-}).listen(8888);
+}
+
+http.createServer(onRequest).listen(8888);
+
+console.log("Server has started.");
