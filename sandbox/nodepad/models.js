28f715165bf87d87bbfdc135965ebaf45549448a
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2012-03-25 14:39:52 +0900

    Part 1

---
+++ sandbox/nodepad/models.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose').Mongoose;
+
+mongoose.model('Document', {
+  properties: ['title', 'data', 'tags'],
+  indexes: ['title']
+});
+
+exports.Document = function(db) {
+  return db.model('Document');
+};
