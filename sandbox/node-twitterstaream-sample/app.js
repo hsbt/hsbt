9fc6fe7937dff9b3175e97a190a74ef783cd0935
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2012-03-28 16:54:37 +0900

    twitter stream

--- sandbox/node-twitterstaream-sample/app.js
+++ sandbox/node-twitterstaream-sample/app.js
@@ -2,10 +2,10 @@ var util = require('util'),
     http = require('http'),
     url = require('url'),
     path = require('path'),
-    fs = require('fs');
+    fs = require('fs'),
+    events = require('events');
 
-http.createServer(function(req, res) {
-  var uri = url.parse(req.url).pathname;
+function load_static_file(uri, res) {
   var filename = path.join(process.cwd(), uri);
 
   path.exists(filename, function(exists) {
@@ -13,6 +13,7 @@ http.createServer(function(req, res) {
       res.writeHead(404, {"Content-Type": "text/plain"});
       res.write("404 Not Found\n");
       res.end();
+      return;
     }
 
     fs.readFile(filename, "binary", function(err, file) {
@@ -28,6 +29,54 @@ http.createServer(function(req, res) {
       res.end();
     });
   });
+}
+
+var twitter_client = http.createClient(80, "api.twitter.com"),
+    tweet_emitter = new events.EventEmitter();
+
+function get_tweets() {
+  var req = twitter_client.request('GET', '/1/statuses/public_timeline.json', {'host': 'api.twitter.com'});
+
+  req.addListener('response', function(res) {
+    var body = "";
+    res.addListener('data', function(data) {
+      body += data;
+    });
+
+    res.addListener('end', function() {
+      var tweets = JSON.parse(body);
+      if(tweets.length > 0) {
+	tweet_emitter.emit('tweets', tweets);
+      }
+    });
+  });
+
+  req.end();
+}
+
+setInterval(get_tweets, 5000);
+
+http.createServer(function(req, res) {
+  var uri = url.parse(req.url).pathname;
+  if(uri === "/stream") {
+    var listener = tweet_emitter.addListener("tweets", function(tweets) {
+      res.writeHead(200, {"Content-Type": "text/plain" });
+      res.write(JSON.stringify(tweets));
+      res.end();
+
+      clearTimeout(timeout);
+    });
+
+    var timeout = setTimeout(function() {
+      res.writeHead(200, {"Content-Type": "text/plain" });
+      res.write(JSON.stringify([]));
+      res.end();
+      tweet_emitter.removeListener("tweets", listener);
+    }, 10000);
+  }
+  else {
+    load_static_file(uri, res);
+  }
 }).listen(8080);
 
-util.puts("Server running at http://localhost:8080/");
+util.puts("Server running at http://localhost:8080");
