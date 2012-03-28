ffcba518d502ccddd80043c8cf7ed6f31f68b173
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2012-03-28 15:03:34 +0900

    simple static server

--- sandbox/node-twitterstaream-sample/app.js
+++ sandbox/node-twitterstaream-sample/app.js
@@ -1,10 +1,33 @@
 var util = require('util'),
-    http = require('http');
+    http = require('http'),
+    url = require('url'),
+    path = require('path'),
+    fs = require('fs');
 
 http.createServer(function(req, res) {
-  res.writeHead(200, {"Content-Type": "text/html"});
-  res.write("Heelo World");
-  res.end();
+  var uri = url.parse(req.url).pathname;
+  var filename = path.join(process.cwd(), uri);
+
+  path.exists(filename, function(exists) {
+    if(!exists) {
+      res.writeHead(404, {"Content-Type": "text/plain"});
+      res.write("404 Not Found\n");
+      res.end();
+    }
+
+    fs.readFile(filename, "binary", function(err, file) {
+      if(err) {
+	res.writeHead(500, {"Content-Type": "text/plain"});
+	res.write(err + "\n");
+	res.end();
+	return;
+      }
+
+      res.writeHead(200);
+      res.write(file, "binary");
+      res.end();
+    });
+  });
 }).listen(8080);
 
 util.puts("Server running at http://localhost:8080/");
