e9eeea1b109688fbc534bf671381f6c531698dc2
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2012-03-29 12:43:49 +0900

    imple file output when fetch suceeded.

--- sandbox/curl-sample/curlsample.cpp
+++ sandbox/curl-sample/curlsample.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include "curl/curl.h"
 
@@ -6,7 +7,16 @@ using namespace std;
 
 static char errorBuffer[CURL_ERROR_SIZE];
 static string buffer;
-static int writer() {
+static int writer(char *data, size_t size, size_t nmemb, string *buffer) {
+  int result = 0;
+
+  if (buffer != NULL) {
+    buffer->append(data, size * nmemb);
+
+    result = size * nmemb;
+  }
+
+  return result;
 }
 
 int usage() {
@@ -27,14 +37,27 @@ int main(int argc, char **argv) {
 
     if(curl) {
       curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, errorBuffer);
-      curl_easy_setopt(curl, CURLOPT_URL, &url);
+      curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
       curl_easy_setopt(curl, CURLOPT_HEADER, 0);
       curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1);
       curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writer);
       curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
-    }
 
+      result = curl_easy_perform(curl);
+
+      curl_easy_cleanup(curl);
+
+      if (result == CURLE_OK) {
+        basic_ofstream<char> ofs(argv[1]);
+        ofs << buffer << endl;
+      } else {
+        cout << "Error: [" << result << "] - " << errorBuffer << endl;
+        return -1;
+      }
+    }
   } else {
     usage();
   }
+
+  return 0;
 }
