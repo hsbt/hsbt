d7f68fb504f3a89fcafc9d02607860cc8b309fcc
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2012-06-21 16:26:20 +0900

    bu

---
+++ sandbox/rails_sorcery_sample/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Sorcery::Application.config.session_store :cookie_store, key: '_sorcery_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Sorcery::Application.config.session_store :active_record_store
