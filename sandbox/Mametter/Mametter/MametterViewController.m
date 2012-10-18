8a5c9852c9e254e2944adb19d7010c24f3bee7d3
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2012-10-18 18:17:42 +0900

    finished chapter 3

--- sandbox/Mametter/Mametter/MametterViewController.m
+++ sandbox/Mametter/Mametter/MametterViewController.m
@@ -11,11 +11,14 @@
 
 @interface MametterViewController ()
 - (void) reloadTweets;
-@property (weak, nonatomic) IBOutlet UIWebView *twitterWebView;
+- (void) handleTwitterData: (NSData*) data
+              urlResponse: (NSHTTPURLResponse*) urlResponse
+                    error: (NSError*) error;
+@property (nonatomic, strong) IBOutlet UITextView *twitterTextView;
 @end
 
 @implementation MametterViewController
-@synthesize twitterWebView;
+@synthesize twitterTextView;
 
 - (void)viewDidLoad
 {
@@ -25,7 +28,7 @@
 
 - (void)viewDidUnload
 {
-    [self setTwitterWebView:nil];
+    [self setTwitterTextView:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
@@ -39,32 +42,59 @@
 {
     if ([SLComposeViewController isAvailableForServiceType: SLServiceTypeTwitter]) {
         SLComposeViewController *tweetVC = [SLComposeViewController composeViewControllerForServiceType: SLServiceTypeTwitter];
-        [tweetVC setInitialText: @"I just finished the first project in iOS SDK Development. #pragsios"];
+        [tweetVC setInitialText: NSLocalizedString (@"I just finished the first project in iOS SDK Development. #pragsios", nil)];
+        tweetVC.completionHandler = ^(SLComposeViewControllerResult result) {
+            if (result == SLComposeViewControllerResultDone) {
+                [self dismissViewControllerAnimated:YES completion:NULL]; [self reloadTweets];
+            }
+        };
         [self presentViewController:tweetVC animated:YES completion:NULL];
-    }else{
-        NSLog (@"Can't send tweet");
     }
 }
 
 -(void) reloadTweets {
-    [self.twitterWebView loadRequest:
-     [NSURLRequest requestWithURL:
-      [NSURL URLWithString:@"http://www.twitter.com/hsbt"]]];
+    NSURL *twitterAPIURL = [NSURL URLWithString:
+                            @"http://api.twitter.com/1/statuses/user_timeline.json"]; NSDictionary *twitterParams = @ {
+        @"screen_name" : @"pragprog", };
+    SLRequest *request = [SLRequest requestForServiceType:SLServiceTypeTwitter
+                                            requestMethod:SLRequestMethodGET
+                                                      URL:twitterAPIURL
+                                               parameters:twitterParams];
+    [request performRequestWithHandler:^(NSData *responseData,
+                                         NSHTTPURLResponse *urlResponse,
+                                         NSError *error) {
+        [self handleTwitterData:responseData
+                    urlResponse:urlResponse
+                          error:error];
+    }];
 }
 
 -(IBAction) handleShowMyTweetsTapped: (id) sender {
-    if ([SLComposeViewController isAvailableForServiceType: SLServiceTypeTwitter]) {
-        SLComposeViewController *tweetVC = [SLComposeViewController composeViewControllerForServiceType: SLServiceTypeTwitter];
-        [tweetVC setInitialText: NSLocalizedString (
-                                                    @"I just finished the first project in iOS SDK Development. #pragsios",
-                                                    nil)];
-        tweetVC.completionHandler = ^(SLComposeViewControllerResult result) {
-            if (result == SLComposeViewControllerResultDone) {
-                [self dismissViewControllerAnimated:YES completion:NULL];
-                [self reloadTweets];
-            }
-        };
-        [self presentViewController:tweetVC animated:YES completion:NULL];
+    [self reloadTweets];
+}
+
+-(void) handleTwitterData: (NSData*) data
+              urlResponse: (NSHTTPURLResponse*) urlResponse
+                    error: (NSError*) error {
+    NSError *jsonError = nil;
+    NSJSONSerialization *jsonResponse = [NSJSONSerialization JSONObjectWithData:data
+                                                                        options:0
+                                                                          error:&jsonError];
+    NSLog (@"jsonResponse: %@", jsonResponse);
+    if (!jsonError && [jsonResponse isKindOfClass:[NSArray class]]) {
+      dispatch_async(dispatch_get_main_queue(), ^{
+          NSArray *tweets = (NSArray*) jsonResponse;
+          NSSortDescriptor *sortByText = [NSSortDescriptor sortDescriptorWithKey:@"text" ascending:YES];
+          NSArray *sortDescriptors = @[sortByText];
+          tweets = [tweets sortedArrayUsingDescriptors:sortDescriptors];
+          for (NSDictionary *tweetDict in tweets) {
+            NSString *tweetText = [NSString stringWithFormat:@"%@: %@ (%@)",
+                                   [tweetDict valueForKeyPath:@"user.name"],
+                                   [tweetDict valueForKey:@"text"],
+                                   [tweetDict valueForKey:@"created_at"]];
+            self.twitterTextView.text = [NSString stringWithFormat:@"%@%@\n\n", self.twitterTextView.text, tweetText];
+        }
+      });
     }
 }
 @end
