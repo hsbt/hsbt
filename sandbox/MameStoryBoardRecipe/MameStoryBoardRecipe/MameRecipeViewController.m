804948e6680b0b7af0fbe655af79bf5c4efded19
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2012-11-28 19:08:29 +0900

    Initial Commit

---
+++ sandbox/MameStoryBoardRecipe/MameStoryBoardRecipe/MameRecipeViewController.m
@@ -0,0 +1,29 @@
+//
+//  MameRecipeViewController.m
+//  MameStoryBoardRecipe
+//
+//  Created by SHIBATA Hiroshi on 2012/11/28.
+//  Copyright (c) 2012年 SHIBATA Hiroshi. All rights reserved.
+//
+
+#import "MameRecipeViewController.h"
+
+@interface MameRecipeViewController ()
+
+@end
+
+@implementation MameRecipeViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ sandbox/MameStoryBoardRecipe/MameStoryBoardRecipe/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MameStoryBoardRecipe
+//
+//  Created by SHIBATA Hiroshi on 2012/11/28.
+//  Copyright (c) 2012年 SHIBATA Hiroshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MameRecipeAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MameRecipeAppDelegate class]));
+    }
+}
