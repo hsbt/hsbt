774289b036f14ba2f05d2e55b2cc49b414812b5c
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2013-02-01 17:45:29 +0900

    rails new: stable version

---
+++ sandbox/whispered/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ActiveKokyaku::Application.config.secret_token = 'e8e8a245687d26ffff369420ea4983d46a31b6e76365601f798cca0745d4eb1469320b8caf0af85e2e5736e02bde7ad5132566cb3319b634219f306e6a5d1824'
