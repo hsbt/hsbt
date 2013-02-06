b144a6961a197e11ab5c6895e25dc0127e6259aa
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2013-02-06 17:11:29 +0900

    tweak searchable attr

--- sandbox/whispered/app/models/inquiry.rb
+++ sandbox/whispered/app/models/inquiry.rb
@@ -2,7 +2,7 @@ class Inquiry < ActiveRecord::Base
   attr_accessible :email, :inq_type, :message, :name
 
   searchable do
-    text :email, :name
-    text :message, :stored => true
+    integer :inq_type
+    text :email, :name, :message
   end
 end
