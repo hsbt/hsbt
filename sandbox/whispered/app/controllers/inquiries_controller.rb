fb9d9e0d94602036652aae58f9eeea3e0f397335
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2013-02-07 18:30:23 +0900

    don't need format.html

--- sandbox/whispered/app/controllers/inquiries_controller.rb
+++ sandbox/whispered/app/controllers/inquiries_controller.rb
@@ -1,26 +1,16 @@
 class InquiriesController < ApplicationController
-  def index
-    @inquiries = Inquiry.all
+  respond_to :html
 
-    respond_to do |format|
-      format.html
-    end
+  def index
+    @inquiries = Inquiry.all(:limit => 5)
   end
 
   def search
     @inquiries = Inquiry.search { fulltext params[:q] }.results
-
-    respond_to do |format|
-      format.html
-    end
   end
 
   def show
     @inquiry = Inquiry.find(params[:id])
-
-    respond_to do |format|
-      format.html
-    end
   end
 
   def edit
@@ -30,12 +20,10 @@ class InquiriesController < ApplicationController
   def update
     @inquiry = Inquiry.find(params[:id])
 
-    respond_to do |format|
-      if @inquiry.update_attributes(params[:inquiry])
-        format.html { redirect_to @inquiry, :notice => 'Inquiry was successfully updated.' }
-      else
-        format.html { render :action => "edit" }
-      end
+    if @inquiry.update_attributes(params[:inquiry])
+      redirect_to @inquiry, :notice => 'Inquiry was successfully updated.'
+    else
+      render :action => "edit"
     end
   end
 end
