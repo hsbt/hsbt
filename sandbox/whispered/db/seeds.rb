cead83cf0080029c0e4cd478767c885be71323a5
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2013-02-08 16:08:10 +0900

    dummy

--- sandbox/whispered/db/seeds.rb
+++ sandbox/whispered/db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+10.times do
+  Inquiry.create!({
+      :name => Forgery::Name.full_name,
+      :email => Forgery(:internet).email_address,
+      :message => Forgery(:lorem_ipsum).words(10),
+      :inq_type => rand(7)
+    })
+end
