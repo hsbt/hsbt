c51474a2842838d3a6f4762023cf04541e403d38
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2013-02-13 17:09:45 +0900

    indexing text fields

--- sandbox/whispered/app/models/inquiry.rb
+++ sandbox/whispered/app/models/inquiry.rb
@@ -2,6 +2,6 @@ class Inquiry < ActiveRecord::Base
   table_name = Settings.inqueries_table_name
 
   searchable do
-    text *columns.select{|c| c.type == :string}.map{|c| c.name.to_sym}
+    text *columns.select{|c| [:string, :text].include? c.type}.map{|c| c.name.to_sym}
   end
 end
