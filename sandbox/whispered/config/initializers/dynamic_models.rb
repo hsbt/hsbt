1dbdbf8de6abde042db4edc41b101e9065a61344
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2013-02-18 15:39:43 +0900

    added link of dynamic model detection.

--- sandbox/whispered/config/initializers/dynamic_models.rb
+++ sandbox/whispered/config/initializers/dynamic_models.rb
@@ -2,8 +2,11 @@ Settings.inquiries_table_names.each do |name|
   class_name = name.singularize.classify
   self.class.const_set class_name, Class.new(ActiveRecord::Base)
   class_name.constantize.class_eval {
+    column_names = columns.select{|c| [:string, :text].include? c.type}.map{|c| c.name.to_sym}
+
+    attr_accessible *column_names if Rails.env.development?
     searchable do
-      text *columns.select{|c| [:string, :text].include? c.type}.map{|c| c.name.to_sym}
+      text *column_names
     end
   }
 end if Settings.inquiries_table_names.present?
