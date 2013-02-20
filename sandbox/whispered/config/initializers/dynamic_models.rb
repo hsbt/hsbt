609e026e9206ada5b266b4c9ad48ddce9031a39d
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2013-02-20 11:01:35 +0900

    showing detail infomation of real database.

--- sandbox/whispered/config/initializers/dynamic_models.rb
+++ sandbox/whispered/config/initializers/dynamic_models.rb
@@ -1,12 +1,14 @@
 Settings.inquiries_table_names.each do |name|
   class_name = name.singularize.classify
   self.class.const_set class_name, Class.new(ActiveRecord::Base)
-  class_name.constantize.class_eval {
-    column_names = columns.select{|c| [:string, :text].include? c.type}.map{|c| c.name.to_sym}
+  class_name.constantize.table_name = name
 
-    attr_accessible *column_names if Rails.env.development?
+  class_name.constantize.class_eval {
+    text_columns = columns.select{|c| [:string, :text].include? c.type}.map{|c| c.name.to_sym}
+
+    attr_accessible *text_columns if Rails.env.development?
     searchable do
-      text *column_names
+      text *text_columns
     end
   }
 end if Settings.inquiries_table_names.present?
