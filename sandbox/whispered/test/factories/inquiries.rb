ab4f74a30f9b4661394b509a4bf6f37aba4d575e
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2013-02-21 15:22:47 +0900

    typo...

--- sandbox/whispered/test/factories/inquiries.rb
+++ sandbox/whispered/test/factories/inquiries.rb
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 FactoryGirl.define do
   factory :inquiry do
-    name Forgery::Nanem.full_name
-    email Forgery(:internet).email_address
-    message Forgery(:lorem_ipsum).words(100)
+    name { Forgery::Name.full_name }
+    email { Forgery(:internet).email_address }
+    message { Forgery(:lorem_ipsum).words(100) }
   end
 end
