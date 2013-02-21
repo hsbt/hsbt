82c80e3b475b4499b96945ae4ef36ecb3d3867ad
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2013-02-21 15:20:11 +0900

    remove travis-solr, and use own configuration of solr

--- sandbox/whispered/test/factories/inquiries.rb
+++ sandbox/whispered/test/factories/inquiries.rb
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 FactoryGirl.define do
   factory :inquiry do
-    name "example"
-    email "example@example.com"
-    message "これはメッセージです"
+    name Forgery::Nanem.full_name
+    email Forgery(:internet).email_address
+    message Forgery(:lorem_ipsum).words(100)
   end
 end
