14685504fa745f8bf0e487c619a654af728985aa
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2013-02-22 14:37:37 +0900

    refactor

--- sandbox/whispered/config/initializers/dynamic_model.rb
+++ sandbox/whispered/config/initializers/dynamic_model.rb
@@ -3,12 +3,12 @@ Settings.inquiries_tables.each do |table|
   self.class.const_set class_name, Class.new(ActiveRecord::Base)
   class_name.constantize.table_name = table.name
 
-  class_name.constantize.class_eval <<-RUBY
-    def self.text_columns
+  class_name.constantize.class_eval do |klass|
+    def klass.text_columns
       columns.select{|c| [:string, :text].include? c.type}.map{|c| c.name.to_sym}
     end
 
-    def self.display_columns
+    def klass.display_columns
       Settings.inquiries_tables.detect{|table| table.name == self.table_name}.display_columns.map(&:to_sym)
     end
 
@@ -16,7 +16,7 @@ Settings.inquiries_tables.each do |table|
     searchable do
       text *text_columns
     end
-  RUBY
+  end
 end if Settings.inquiries_tables.present? && !ENV['TRAVIS_INIT']
 
 module DynamicModel
