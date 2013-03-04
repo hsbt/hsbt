51baf4727a8d2fc3dba14c269ac08062b83e5274
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2013-03-04 16:42:23 +0900

    rake rails:update

--- sandbox/whispered/config/initializers/secret_token.rb
+++ sandbox/whispered/config/initializers/secret_token.rb
@@ -2,6 +2,11 @@
 
 # Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Whispered::Application.config.secret_token = 'e8e8a245687d26ffff369420ea4983d46a31b6e76365601f798cca0745d4eb1469320b8caf0af85e2e5736e02bde7ad5132566cb3319b634219f306e6a5d1824'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Whispered::Application.config.secret_key_base = '34e485fc345a5fa4ea0a0a9858873b4136ebe2e992981c523f3786857653c67a23ea812e6434588d99d88cf61f4c50bc9f72a42c236fd5f6e414791a05d35e64'
