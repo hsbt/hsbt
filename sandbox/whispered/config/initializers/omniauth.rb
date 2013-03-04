d526a02454ca0d7e585b5fa144e2f097768a01a8
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2013-03-04 14:56:52 +0900

    nil handle

--- sandbox/whispered/config/initializers/omniauth.rb
+++ sandbox/whispered/config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, Settings.github.key, Settings.github.secret
 end
-OmniAuth.config.full_host = Settings.omni_auth.full_host if Settings.omni_auth.full_host
+OmniAuth.config.full_host = Settings.omni_auth.full_host if Settings.omni_auth.try(:full_host)
