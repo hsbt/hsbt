fd0ab030f9fdcb316ec57c294e97240adf09e11f
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2013-03-04 14:47:07 +0900

    added workaround for OmniAuth

--- sandbox/whispered/config/initializers/omniauth.rb
+++ sandbox/whispered/config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, Settings.github.key, Settings.github.secret
 end
+OmniAuth.config.full_host = Settings.omni_auth.full_host if Settings.omni_auth.full_host
