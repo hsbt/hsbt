6264078faa6c7ecee256c739e002643f2d3a5f02
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2013-03-13 18:44:31 +0900

    permission

--- sandbox/whispered-puppet/modules/rbenv/manifests/init.pp
+++ sandbox/whispered-puppet/modules/rbenv/manifests/init.pp
@@ -12,6 +12,7 @@ class rbenv ($user="whispered") {
     path    => "/usr/bin",
     command => "git clone git://github.com/sstephenson/rbenv.git /home/$user/.rbenv",
     unless  => "test -d /home/$user/.rbenv",
+    user => $user,
     require => Package['git'],
   }
 
@@ -19,12 +20,14 @@ class rbenv ($user="whispered") {
     path    => "/usr/bin",
     command => "git clone git://github.com/sstephenson/ruby-build.git /home/$user/.rbenv/plugins/ruby-build",
     unless  => "test -d /home/$user/.rbenv/plugins/ruby-build",
+    user => $user,
     require => Exec["rbenv"];
   }
 
   file { "/home/$user/.bash_profile":
     source  => "puppet:///modules/rbenv/.bash_profile",
     ensure => present,
+    user => $user,
     require => User[$user],
   }
 }
