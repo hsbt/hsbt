bf4c4c5238bc99b558a18ac8752cd77d41bb8dc5
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2013-03-14 18:09:24 +0900

    added upstream configuration for nginx

--- sandbox/whispered-puppet/modules/nginx/manifests/init.pp
+++ sandbox/whispered-puppet/modules/nginx/manifests/init.pp
@@ -1,9 +1,15 @@
-class nginx {
+class nginx ($user="whispered"){
   package { "nginx":
     ensure => present,
+  }
+  file { "/etc/nginx/nginx.conf":
+    ensure => present,
+    content => template("nginx/nginx.conf"),
+    require => Package['nginx'],
   }
   file { "/etc/nginx/conf.d/default.conf":
     ensure => present,
     content => template("nginx/default.conf"),
+    require => Package['nginx'],
   }
 }
