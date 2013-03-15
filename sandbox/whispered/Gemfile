dc93f867e2d2a4d27419f0fb9c286ae20d4f287b
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2013-03-15 12:49:39 +0900

    replace capistrano-puma

--- sandbox/whispered/Gemfile
+++ sandbox/whispered/Gemfile
@@ -31,7 +31,6 @@ end
 group :development, :test do
   gem 'pit', :require => false
   gem 'capistrano', :require => false
-  gem 'capistrano-puma', :require => false
   gem 'capistrano_colors', :require => false
 
   gem 'sunspot_solr'
--- sandbox/whispered/config/deploy.rb
+++ sandbox/whispered/config/deploy.rb
@@ -2,7 +2,6 @@
 
 require 'capistrano_colors'
 require 'bundler/capistrano'
-require 'capistrano-puma'
 
 set :default_environment, {
   'PATH' => "$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH"
@@ -45,4 +44,30 @@ namespace :deploy do
 
   after 'deploy:finalize_update', 'deploy:update_symlink'
   after 'deploy:update', 'deploy:cleanup'
+end
+
+set :stage, 'production'
+set :shared_children, shared_children << 'tmp/sockets'
+
+puma_sock    = "unix://#{shared_path}/sockets/puma.sock"
+puma_control = "unix://#{shared_path}/sockets/pumactl.sock"
+puma_state   = "#{shared_path}/sockets/puma.state"
+puma_log     = "#{shared_path}/log/puma-#{stage}.log"
+
+namespace :deploy do
+  desc "Start the application"
+  task :start do
+    run "cd #{current_path} && RAILS_ENV=#{stage} && bundle exec puma -d -b '#{puma_sock}' -e #{stage} -t2:4 --control '#{puma_control}' -S #{puma_state} >> #{puma_log} 2>&1 &", :pty => false
+  end
+
+  desc "Stop the application"
+  task :stop do
+    run "cd #{current_path} && RAILS_ENV=#{stage} && bundle exec pumactl -S #{puma_state} stop"
+  end
+
+  desc "Restart the application"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    stop
+    start
+  end
 end
