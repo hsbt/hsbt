dd35816e01dadd0a6560c14951368376aed3b99b
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2013-03-15 12:00:17 +0900

    temporary

--- sandbox/whispered-puppet/modules/puma/manifests/init.pp
+++ sandbox/whispered-puppet/modules/puma/manifests/init.pp
@@ -1,14 +1,9 @@
-class puma {
+class puma ($user="whispered"){
   file { "/etc/init.d/puma":
     owner => root,
     group => root,
     mode => "755",
     source => "puppet:///modules/puma/puma"
-  }
-
-  service { "puma":
-    ensure => "running",
-    require => File["/etc/init.d/puma"],
   }
 
   file { "/usr/local/bin/run-puma":
@@ -18,10 +13,22 @@ class puma {
     source => "puppet:///modules/puma/run-puma",
   }
 
-  exec { "puma.conf":
-    command => "touch /etc/puma.conf",
-    unless => "test -f /etc/puma.conf",
-    path => "/bin",
-    require => [Service["puma"], File["/usr/local/bin/run-puma"]]
-  }
+  # $apppath = "/home/$user/app/whispered/current"
+  # exec { "puma.conf":
+  #   command => "/etc/init.d/puma add $apppath $user $apppath/config/puma.rb $apppath/log/puma.log",
+  #   if => "test -f $apppath/config/puma.rb",
+  #   unless => "test -f /etc/puma.conf",
+  #   path => "/usr/bin",
+  #   require => [
+  #     File["/usr/local/bin/run-puma"],
+  #     File["/etc/init.d/puma"],
+  #   ]
+  # }
+
+  # service { "puma":
+  #   ensure => "running",
+  #   require => [
+  #     Exec["puma.conf"],
+  #   ]
+  # }
 }
