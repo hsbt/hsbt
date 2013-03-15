1dde1f9865a7bcd802068663307c75ef6b43fb47
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2013-03-15 11:33:33 +0900

    added service

--- sandbox/whispered-puppet/modules/nginx/manifests/init.pp
+++ sandbox/whispered-puppet/modules/nginx/manifests/init.pp
@@ -2,14 +2,24 @@ class nginx ($user="whispered"){
   package { "nginx":
     ensure => present,
   }
+
   file { "/etc/nginx/nginx.conf":
     ensure => present,
     content => template("nginx/nginx.conf"),
     require => Package['nginx'],
   }
+
   file { "/etc/nginx/conf.d/default.conf":
     ensure => present,
     content => template("nginx/default.conf"),
     require => Package['nginx'],
+  }
+
+  service { "nginx":
+    ensure => "running",
+    require => [
+      File["/etc/nginx/nginx.conf"],
+      File["/etc/nginx/conf.d/default.conf"]
+    ]
   }
 }
