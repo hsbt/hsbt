0691a91c6d2da744b25d0750df08fb650212a59c
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2013-09-10 17:01:59 +0900

    require provider attributes when using directly download

--- sandbox/whispered-puppet/modules/elasticsearch/manifests/install.pp
+++ sandbox/whispered-puppet/modules/elasticsearch/manifests/install.pp
@@ -8,5 +8,6 @@ class elasticsearch::install {
     ensure => latest,
     source => 'https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.3.noarch.rpm',
     require => Package['java-1.7.0-openjdk'],
+    provider => rpm,
   }
 }
