d8e2b458d93d397212e2c4e2e1f6fa53fdc8aea4
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2014-03-25 23:09:41 +0900

    ignoded 1.9 and jruby

--- ruby-update.rb
+++ ruby-update.rb
@@ -2,7 +2,10 @@
 require 'pathname'
 
 Dir.glob(File.expand_path("~/.rbenv/versions/*")).each do |v|
-  system "rbenv install #{Pathname(v).basename.to_s} --force"
+  if !v.include?("1.9.3-dev") && !v.include?("jruby-1.7.11")
+    system "rbenv install #{Pathname(v).basename.to_s} --force"
+  end
+
   gem_cmd = "#{v}/bin/gem"
   system "#{gem_cmd} update --system"
   system "yes | #{gem_cmd} update"
