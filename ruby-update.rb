1de4c77ab42d48ae0a9e0e1c6fb093431fd7e6b0
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2014-04-23 09:59:07 +0900

    ignored daily build

--- ruby-update.rb
+++ ruby-update.rb
@@ -2,11 +2,9 @@
 require 'pathname'
 
 Dir.glob(File.expand_path("~/.rbenv/versions/*")).each do |v|
-  if !v.include?("jruby")
-    system "rbenv install #{Pathname(v).basename.to_s} --force"
-  end
-
   gem_cmd = "#{v}/bin/gem"
+
+  system "rbenv install #{Pathname(v).basename.to_s} --force" if v.include?("2.2.0")
   system "#{gem_cmd} update --system"
   system "yes | #{gem_cmd} update"
   system "yes | #{gem_cmd} cleanup"
