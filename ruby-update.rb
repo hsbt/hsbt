b33dcc38ff99b69b6b4915a90194f4be70ae13f9
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2014-05-14 20:40:55 +0900

    build all rubies

--- ruby-update.rb
+++ ruby-update.rb
@@ -4,7 +4,7 @@ require 'pathname'
 Dir.glob(File.expand_path("~/.rbenv/versions/*")).each do |v|
   gem_cmd = "#{v}/bin/gem"
 
-  system "rbenv install #{Pathname(v).basename.to_s} --force" if v.include?("2.2.0")
+  system "rbenv install #{Pathname(v).basename.to_s} --force"
   system "#{gem_cmd} update --system"
   system "yes | #{gem_cmd} update"
   system "yes | #{gem_cmd} cleanup"
