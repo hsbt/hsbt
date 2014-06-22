251647d095a9eefe57cff486d5fd2ec6a1d41e06
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2014-06-22 18:40:26 +0900

    remove obsoleted configuration with emacs 24.3

--- archives/dot.emacs.d/inits/25-killring.el
+++ archives/dot.emacs.d/inits/25-killring.el
@@ -1,7 +1,3 @@
-;; クリップボード履歴を kill-ring 送りにする
-(require 'clipboard-to-kill-ring)
-(clipboard-to-kill-ring t)
-
 ;; 履歴の拡張
 (require 'recentf-ext)
 (setq recentf-max-saved-items 3000)
--- archives/dot.emacs.d/inits/31-multiterm.el
+++
@@ -1,3 +0,0 @@
-(require 'egg)
-(when (require 'multi-term nil t)
-  (setq multi-term-program "/usr/local/bin/zsh"))
