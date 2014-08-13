e583048d00875808e41e316527dce837b4012bc9
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2014-08-13 09:25:14 +0900

    tweak

--- ruby-update.rb
+++ ruby-update.rb
@@ -2,12 +2,14 @@
 require 'pathname'
 
 Dir.glob(File.expand_path("~/.rbenv/versions/*")).each do |v|
-  next unless v.include?('dev')
+  if %w(2.1.0-dev 2.2.0-dev).include? v
+    system "rbenv install #{Pathname(v).basename.to_s} --force"
+  end
 
-  gem_cmd = "#{v}/bin/gem"
-
-  system "rbenv install #{Pathname(v).basename.to_s} --force"
-  system "#{gem_cmd} update --system"
-  system "yes | #{gem_cmd} update"
-  system "yes | #{gem_cmd} cleanup"
+  if v.include?('dev')
+    gem_cmd = "#{v}/bin/gem"
+    system "#{gem_cmd} update --system"
+    system "yes | #{gem_cmd} update"
+    system "yes | #{gem_cmd} cleanup"
+  end
 end
