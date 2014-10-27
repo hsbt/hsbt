35ecbf10c3d5ea87bdd30677b83a7ac094563133
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2014-10-27 16:29:44 +0900

    added bootstrap scripts

---
+++ boxes/scripts/bootstrap_apt.sh
@@ -0,0 +1,1 @@
+sudo apt-get install build-essential libreadline6 libreadline6-dev openssl libssl-dev autoconf bison git subversion libffi-dev libgdbm-dev
---
+++ boxes/scripts/bootstrap_yum.sh
@@ -0,0 +1,2 @@
+sudo yum group install ‘Development Tools’
+sudo yum install openssl-devel zlib-devel
