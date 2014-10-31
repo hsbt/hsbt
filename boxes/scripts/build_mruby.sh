15b3b14fe69e8b973b840ebe63ca076e145c3cd9
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2014-10-31 14:52:36 +0900

    added package task

--- boxes/scripts/build_mruby.sh
+++ boxes/scripts/build_mruby.sh
@@ -8,3 +8,6 @@ make generate_gems_config
 cd /vagrant/github.com/nginx/nginx
 ./configure --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log --pid-path=/var/run/nginx.pid --lock-path=/var/run/nginx.lock --http-client-body-temp-path=/var/cache/nginx/client_temp --http-proxy-temp-path=/var/cache/nginx/proxy_temp --http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp --http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp --http-scgi-temp-path=/var/cache/nginx/scgi_temp --user=nginx --group=nginx --with-http_ssl_module --with-http_realip_module --with-http_addition_module --with-http_sub_module --with-http_dav_module --with-http_flv_module --with-http_mp4_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_random_index_module --with-http_secure_link_module --with-http_stub_status_module --with-mail --with-mail_ssl_module --with-file-aio --with-ipv6 --with-cc-opt='-O2 -g -pipe -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector --param=ssp-buffer-size=4 -m64 -mtune=generic' --add-module=/vagrant/github.com/matsumoto-r/ngx_mruby --add-module=/vagrant/github.com/matsumoto-r/ngx_mruby/dependence/ngx_devel_kit
 make
+
+tar czf objs/nginx.tar.gz objs/nginx
+mv objs/nginx.tar.gz /vagrant
