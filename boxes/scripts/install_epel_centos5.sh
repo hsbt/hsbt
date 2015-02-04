9126b2036fde05abd3c26bca241bec0f47fce4e1
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2015-02-04 21:49:56 +0900

    ignored exit when already installed epel

--- boxes/scripts/install_epel_centos5.sh
+++ boxes/scripts/install_epel_centos5.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sudo rpm -Uvh http://ftp-srv2.kddilabs.jp/Linux/distributions/fedora/epel/5/x86_64/epel-release-5-4.noarch.rpm
+sudo rpm --force -Uvh http://ftp-srv2.kddilabs.jp/Linux/distributions/fedora/epel/5/x86_64/epel-release-5-4.noarch.rpm
