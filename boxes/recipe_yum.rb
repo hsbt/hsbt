281df9ef69e2c44c7d771ce7821937d1cfbb86ac
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2015-02-06 16:27:37 +0900

    fix wrong usage of execute block

--- boxes/recipe_yum.rb
+++ boxes/recipe_yum.rb
@@ -4,5 +4,8 @@ end
 
 execute 'groupinstall' do
   command 'sudo yum groupinstall "Development Tools" -y'
+end
+
+execute 'ruby-devel' do
   command 'sudo yum install openssl-devel zlib-devel -y'
 end
