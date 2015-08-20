e9ee6a18ca4d318548ffba886245f76ca4909ea2
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2015-08-21 07:10:48 +0900

    Update ruby19-hash-syntax.sh

--- ruby19-hash-syntax.sh
+++ ruby19-hash-syntax.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-perl -pi -e 's/\{:([\w\d_]+)(\s*)=>/{\1:/g' **/*.rb
-perl -pi -e 's/\(:([\w\d_]+)(\s*)=>/(\1:/g' **/*.rb
-perl -pi -e 's/\ :([\w\d_]+)(\s*)=>/ \1:/g' **/*.rb
+perl -pi -e 's/([{( ]):(\w+)\s*=>/\1\2:/g' **/*.rb
