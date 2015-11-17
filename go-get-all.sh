a953d3f8f595da3c8dc508cb1f8eede71e75cced
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2015-11-17 15:40:21 +0900

    dirty update

---
+++ go-get-all.sh
@@ -0,0 +1,22 @@
+#/usr/bin/bash
+
+go get -u github.com/github/hub
+go get -u github.com/zimbatm/direnv
+go get -u github.com/ddollar/forego
+go get -u github.com/typester/gh-open
+go get -u github.com/mattn/gom
+go get -u github.com/heroku/hk
+go get -u github.com/motemen/hub-pr
+go get -u github.com/nitrous-io/tug
+go get -u github.com/motemen/ghq
+go get -u github.com/mackerelio/mkr
+go get -u github.com/mitchellh/gox
+go get -u github.com/hashicorp/vault
+go get -u github.com/hashicorp/terraform
+go get -u github.com/monochromegane/the_platinum_searcher/cmd/pt
+go get -u github.com/peco/peco/cmd/peco
+go get -u github.com/cubicdaiya/nginx-build
+go get -u github.com/github/git-lfs
+go get -u github.com/motemen/github-list-starred
+go get -u github.com/sona-tar/ghs
+go get -u github.com/tools/godep`
