716527e738bb9eb4aa82e98056717da2f746f0e1
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2015-12-26 08:15:33 +0900

    typo fix

--- merge-rubygems.sh
+++ merge-rubygems.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-rm -rf lib/rubygems test/rubygtems lib/rubygems.rb
+rm -rf lib/rubygems test/rubygems lib/rubygems.rb
 cp -rf ../../rubygems/rubygems/lib/rubygems ./lib
 cp -rf ../../rubygems/rubygems/lib/rubygems.rb ./lib
 cp -rf ../../rubygems/rubygems/test/rubygems ./test
