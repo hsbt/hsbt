15b95d1c87a093d2c45c4db946273c98910d3570
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2016-02-09 13:26:52 +0900

    init

---
+++ sandbox/rake-issue-chdir/check_no_expansion.rb
@@ -0,0 +1,6 @@
+p ARGV
+if ARGV[0] != ARGV[1]
+  exit 0
+else
+  exit 1
+end
---
+++ sandbox/rake-issue-chdir/rake_issue.rb
@@ -0,0 +1,16 @@
+def sh(*cmd)
+  res = system(*cmd)
+  status = $?
+  p [res, status]
+end
+
+RUBY = ENV['RUBY'] || File.join(
+    RbConfig::CONFIG['bindir'],
+    RbConfig::CONFIG['ruby_install_name'] + RbConfig::CONFIG['EXEEXT'])
+
+ENV['RAKE_TEST_SH'] = 'someval'
+
+sh RUBY, 'check_no_expansion.rb', '$RAKE_TEST_SH', 'someval'
+
+Dir.chdir 'test'
+sh RUBY, 'check_no_expansion.rb', '$RAKE_TEST_SH', 'someval'
