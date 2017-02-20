8db06b904551a09977d271a7e0728aea70de3779
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2017-02-20 19:53:31 +0900

    convert script for dayone2 to notes app

---
+++ dayone2note.rb
@@ -0,0 +1,16 @@
+require 'json'
+require 'fileutils'
+
+Dir.glob('*.json') do |file|
+  json = JSON.parse(File.read(file))
+  dir = file.gsub(/.json/, '')
+
+  FileUtils.mkdir_p(dir)
+  Dir.chdir(dir) do
+    json['entries'].each do |entry|
+      open("#{entry['uuid']}.txt", "w") do |f|
+        f.write entry['text']
+      end
+    end
+  end
+end
