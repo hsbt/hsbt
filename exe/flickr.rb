0d566bde42c630be80a5b7c9fbf417385e10652e
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2018-12-13 09:31:55 +0900

    Added script for organize date structured directory

---
+++ exe/flickr.rb
@@ -0,0 +1,22 @@
+require 'exifr'
+require 'time'
+require 'fileutils'
+require 'pathname'
+
+Dir.glob("data/*").each do |f|
+  exif = Exif::Data.new(File.open(f))
+  time = if exif.date_time_original
+           exif.date_time_original
+         elsif exif.date_time
+           exif.date_time
+         else
+          p exif
+          raise Exif::NotReadable
+         end
+  t = Time.parse(time[0..9].split(":").join("/"))
+  dir = "#{t.year}/#{'%02d' % t.month}"
+  FileUtils.mkdir_p(dir)
+  FileUtils.mv f, File.join(dir, Pathname(f).basename.to_s)
+rescue Exif::NotReadable
+  next
+end
