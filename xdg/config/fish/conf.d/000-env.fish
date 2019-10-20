0150c06fe9934ddf94e168d23d6d8ca8a6704f8d
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2019-10-21 07:53:34 +0900

    Add helm config and remove the duplicated fish_prompt configuration.

--- xdg/config/fish/conf.d/000-env.fish
+++ xdg/config/fish/conf.d/000-env.fish
@@ -23,6 +23,7 @@ set -x AWS_CONFIG_FILE $XDG_CONFIG_HOME/aws/config
 set -x MAVEN_USER_HOME $XDG_CACHE_HOME/maven
 set -x MAVEN_CONFIG "-gs $XDG_CONFIG_HOME/maven/settings.xml"
 set -x PYLINTHOME $XDG_CACHE_HOME/pylint
+set -x HELM_HOME $XDG_CONFIG_HOME/helm
 
 set -x LANG en_US.UTF-8
 set -x EDITOR vim
