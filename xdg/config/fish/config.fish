63c22070d8eb8ab4c76ca309897a162691ecc6da
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2020-02-26 19:43:58 +0900

    aliases xdg compliant with wget

--- xdg/config/fish/config.fish
+++ xdg/config/fish/config.fish
@@ -12,6 +12,7 @@ alias bx "bundle exec"
 alias all-ruby "docker run --rm -t rubylang/all-ruby /all-ruby/all-ruby"
 alias s3cmd "s3cmd --config $HOME/.config/s3cmd"
 alias mvn "mvn -gs $HOME/.config/maven/settings.xml"
+alias wget 'wget --hsts-file="$XDG_CACHE_HOME/wget-hsts"'
 
 function export
   set arr (echo $argv|tr = \n)
