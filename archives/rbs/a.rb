df8e093ae251869aa23ce48e33326bd354e31f14
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2020-08-03 15:02:46 +0900

    rbs handson

---
+++ archives/rbs/a.rb
@@ -0,0 +1,24 @@
+class Commit
+  attr_reader :sha
+end
+
+class Committer
+  attr_reader :name, :email, :github_user, :commits
+  
+  def initialize(name:, email:, github_user:)
+    @name = name
+    @email = email
+    @github_user = github_user
+    @commits = []
+  end
+  
+  def each_commit(&block)
+    if block
+      commits.each(&block)
+    else
+      enum_for :each_commit
+    end
+  end
+end
+
+committer = Committer.new(name: :soutaro, email: nil, github_user: "soutaro")
