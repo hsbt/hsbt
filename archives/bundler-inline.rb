2ce90d0b99ecc4aafef3d7f26e0f3a178ed96fae
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2020-08-31 09:30:46 +0900

    sandbox

---
+++ archives/bundler-inline.rb
@@ -0,0 +1,15 @@
+# main.rb
+
+# frozen_string_literal: true
+
+require "bundler/inline"
+
+gemfile(true) do
+  source "https://rubygems.org"
+
+  git_source(:github) { |repo| "https://github.com/#{repo}.git" }
+
+  gem "rack-test"
+end
+
+require "rack/test" 
