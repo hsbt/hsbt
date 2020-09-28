60d628da0119b17ba30a5ed3e2d7e6e3b8fd1535
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2020-09-28 18:26:42 +0900

    Use GitHub Container Registry

--- xdg/config/fish/config.fish
+++ xdg/config/fish/config.fish
@@ -9,7 +9,7 @@ alias e "code-insiders -a ."
 alias en "code-insiders ."
 alias bx "bundle exec"
 
-alias all-ruby "docker run --rm -t rubylang/all-ruby /all-ruby/all-ruby"
+alias all-ruby "docker run --rm -t ghcr.io/ruby/all-ruby /all-ruby/all-ruby"
 alias s3cmd "s3cmd --config $HOME/.config/s3cmd"
 alias mvn "mvn -gs $HOME/.config/maven/settings.xml"
 alias wget 'wget --hsts-file="$XDG_CACHE_HOME/wget-hsts"'
