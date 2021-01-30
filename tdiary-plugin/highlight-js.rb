5c94238c0d8c93cbd488782cf5fd2867928ea5b3
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2021-01-30 10:29:23 +0900

    Import local plugin for tDiary

---
+++ tdiary-plugin/highlight-js.rb
@@ -0,0 +1,7 @@
+add_header_proc do
+<<-STYLE
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/default.min.css">
+<script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js"></script>
+<script>hljs.initHighlightingOnLoad();</script>
+STYLE
+end
