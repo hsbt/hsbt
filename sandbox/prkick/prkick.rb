98214f797dbb7c9c81f4335e8b3d1a9270949c5c
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2022-03-25 15:18:01 +0900

    dependabot workflow

---
+++ sandbox/prkick/prkick.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+
+require "bundler/inline"
+require "fileutilsg"
+
+gemfile do
+  source "https://rubygems.org"
+  gem "octokit"
+end
+
+class GitHub
+  class << self
+    def com
+      Octokit.reset!
+
+      Octokit.configure do |c|
+        c.access_token = ENV['GITHUB_ACCESS_TOKEN']
+        c.auto_paginate = true
+        c.per_page = 100
+      end
+
+      @_com ||= Octokit::Client.new
+    end
+  end
+end
+
+REPOSITORIES = [
+  'hsbt/hsbt',
+]
+
+REPOSITORIES.each do |repo|
+  Dir.chdir(File.expand_path("~/Documents/github.com/#{repo}")) do
+    puts "Updating #{repo}..."
+    system "git switch master"
+    system "git reset origin/master --hard"
+    system "git pull --rebase"
+    system "git checkout -b add-dependabot"
+    FileUtils.mkdir_p(".github")
+    FileUtils.cp("dependabot.yml", ".github")
+    system "git commit -am 'Added dependabot'"
+    system "git push"
+  end
+end
