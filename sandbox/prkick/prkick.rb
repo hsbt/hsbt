debc450a47978e8c21d6e9009e436dde6fb2622b
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2022-03-25 16:27:16 +0900

    it works

--- sandbox/prkick/prkick.rb
+++ sandbox/prkick/prkick.rb
@@ -1,44 +1,22 @@
 #!/usr/bin/env ruby
 
-require "bundler/inline"
 require "fileutils"
 
-gemfile do
-  source "https://rubygems.org"
-  gem "octokit"
-end
-
-class GitHub
-  class << self
-    def com
-      Octokit.reset!
-
-      Octokit.configure do |c|
-        c.access_token = ENV['GITHUB_ACCESS_TOKEN']
-        c.auto_paginate = true
-        c.per_page = 100
-      end
-
-      @_com ||= Octokit::Client.new
-    end
-  end
-end
-
 REPOSITORIES = [
-  'hsbt/hsbt',
+  "ruby/ruby",
 ]
 
+template = File.expand_path("../dependabot.yml", __FILE__)
+
 REPOSITORIES.each do |repo|
+  system "ghq get https://github.com/#{repo}"
   Dir.chdir(File.expand_path("~/Documents/github.com/#{repo}")) do
-    puts "Updating #{repo}..."
-    system "git switch master"
-    system "git reset origin/master --hard"
-    system "git pull --rebase"
     system "git checkout -b add-dependabot"
     FileUtils.mkdir_p(".github")
-    FileUtils.cp(File.expand_path("../dependabot.yml", __FILE__), ".github")
+    FileUtils.cp(template, ".github")
     system "git add .github"
     system "git commit -m 'Added dependabot'"
     system "git push"
+    system "gh pr create --base master --head ruby:add-dependabot --title 'Added dependabot.yml for actions' --body ''"
   end
 end
