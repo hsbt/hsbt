98bfe64c531be65144c9700e4a3e87d6c9d45e54
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2022-04-20 09:31:15 +0900

    Update the latest usage

--- xdg/config/fish/conf.d/000-env.fish
+++ xdg/config/fish/conf.d/000-env.fish
@@ -1,6 +1,7 @@
 set -x XDG_CONFIG_HOME $HOME/.config
-set -x XDG_DATA_HOME $HOME/.local/share
 set -x XDG_CACHE_HOME $HOME/.cache
+set -x XDG_DATA_HOME $HOME/.local/share
+set -x XDG_STATE_HOME $HOME/.local/state
 
 set -x IRBRC $XDG_CONFIG_HOME/irb/irbrc
 set -x GEMRC $XDG_CONFIG_HOME/gem/gemrc
@@ -17,6 +18,7 @@ set -x NPM_CONFIG_USERCONFIG $XDG_CONFIG_HOME/npm/npmrc
 set -x GOPATH $XDG_DATA_HOME/go
 set -x GOBIN $HOME/.local/bin
 set -x AWS_SHARED_CREDENTIALS_FILE $XDG_CONFIG_HOME/aws/credentials
+set -x DOCKER_CONFIG $XDG_CONFIG_HOME/docker
 set -x AWS_CONFIG_FILE $XDG_CONFIG_HOME/aws/config
 set -x MAVEN_USER_HOME $XDG_CACHE_HOME/maven
 set -x MAVEN_CONFIG "-gs $XDG_CONFIG_HOME/maven/settings.xml"
@@ -26,6 +28,8 @@ set -x KREW_ROOT $XDG_DATA_HOME/krew
 set -x VIMINIT ":source $XDG_CONFIG_HOME"/vim/vimrc
 set -x NODE_REPL_HISTORY $XDG_DATA_HOME/node_repl_history
 set -x VSCODE_PORTABLE "$XDG_DATA_HOME"/vscode
+set -x _ZO_DATA_DIR $XDG_DATA_HOME/zoxide
+set -x TEALDEER_CACHE_DIR $XDG_CACHE_HOME/tldr
 
 set -x LANG en_US.UTF-8
 set -x GPG_TTY (tty)
@@ -37,10 +41,7 @@ set -x GO111MODULE on
 set -x RUBY_CODESIGN hsbt
 set -x RUBY_CONFIGURE_OPTS --disable-install-doc
 set -x PKG_CONFIG_PATH /opt/homebrew/opt/imagemagick@6/lib/pkgconfig:/usr/lib/pkgconfig
-set -x _ZO_DATA_DIR $XDG_DATA_HOME/zoxide
-set -x TEALDEER_CACHE_DIR $XDG_CACHE_HOME/tldr
 set -x CLOUDSDK_PYTHON /opt/homebrew/opt/python@3.8/libexec/bin/python
-set -x CARGO_NET_GIT_FETCH_WITH_CLI true
 
 set -x PATH $GOBIN $CARGO_HOME/bin $RBENV_ROOT/bin $KREW_ROOT/bin $PATH
 set -x PATH /opt/homebrew/bin /opt/homebrew/sbin $PATH
