4bcabfe076bd388345462f970f640d29d6e8c73d
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2022-04-26 07:45:33 +0900

    Handling error with empty repository

--- sandbox/prkick2/prkick.rb
+++ sandbox/prkick2/prkick.rb
@@ -27,8 +27,14 @@ repos.each do |repo|
   next unless !!client.tree(repo, "HEAD")[:tree].find{|o| o[:path] == "Gemfile" }
   next if !!client.tree(repo, "HEAD")[:tree].find{|o| o[:path] == ".github/dependabot.yml" }
 
+  puts repo
+
   default_branch = client.repo(repo).default_branch
   client.create_ref(repo, "refs/heads/#{branch_name}", client.ref(repo, "heads/#{default_branch}").object.sha)
   client.create_contents(repo, ".github/dependabot.yml", "init dependabot.yml", content, :branch => branch_name)
   client.create_pull_request(repo, default_branch, branch_name, pr_title)
+
+  puts "done"
+rescue Octokit::Conflict
+  puts "#{repo} is failed to create a dependabot.yml"
 end
