768aa982e7e5f270b0b8b1fdfa824a57d6d3f949
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2022-04-25 18:28:48 +0900

    Rewrite with pure-ruby

--- sandbox/prkick2/prkick.rb
+++ sandbox/prkick2/prkick.rb
@@ -1,22 +1,37 @@
 #!/usr/bin/env ruby
 
-require "fileutils"
+require "bundler/inline"
+
+gemfile do
+  source "https://rubygems.org"
+  gem "octokit"
+end
+
+require "octokit"
+
+Octokit.configure do |c|
+  c.web_endpoint = ENV["GITHUB_HOST"]
+  c.api_endpoint = "#{ENV["GITHUB_HOST"]}/api/v3"
+  c.access_token = ENV['GITHUB_TOKEN']
+  c.auto_paginate = true
+  c.per_page = 100
+end
 
 REPOSITORIES = [
-  "ruby/ruby",
+  "hsbt/hsbt",
 ]
 
-template = File.expand_path("../dependabot.yml", __FILE__)
+client = Octokit::Client.new
+branch_name = "dependabot/init"
+content = File.read(File.expand_path("../dependabot.yml", __FILE__))
 
 REPOSITORIES.each do |repo|
-  system "ghq get https://github.com/#{repo}"
-  Dir.chdir(File.expand_path("~/Documents/github.com/#{repo}")) do
-    system "git checkout -b add-dependabot"
-    FileUtils.mkdir_p(".github")
-    FileUtils.cp(template, ".github")
-    system "git add .github"
-    system "git commit -m 'Added dependabot'"
-    system "git push"
-    system "gh pr create --base master --head ruby:add-dependabot --title 'Added dependabot.yml for actions' --body ''"
+  unless !!client.tree(repo, "HEAD")[:tree].find{|o| o[:path] == "Gemfile" }
+    next
   end
+
+  default_branch = client.repo(repo).default_branch
+  client.create_ref(repo, "refs/heads/#{branch_name}", client.ref(repo, "heads/#{default_branch}").object.sha)
+  client.create_contents(repo, ".github/dependabot.yml", "init dependabot", content, :branch => branch_name)
+  client.create_pull_request(repo, default_branch, branch_name, "Create a dependabot.yml")
 end
