4b29732fdf1f03d63779c1db379e9e68404a6bf5
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2022-06-09 14:17:08 +0900

    boxes is sandbox now

--- boxes/scripts/install_libmemcached_centos5.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-sudo rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-5.rpm
-sudo yum --enablerepo=remi --disablerepo=epel install libmemcached libmemcached-devel -y
