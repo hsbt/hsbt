8f021f97640457d73c4eee71b9ec1bdb947fbeda
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2022-08-28 20:08:54 +0900

    Revert "Use Docker desktop again"
    This reverts commit 9a915579816c1f9f53d5c9234831a2ca333472d8.

--- toolbox/xdg/config/fish/config.fish
+++ toolbox/xdg/config/fish/config.fish
@@ -2,10 +2,11 @@ alias ls lsd
 alias l ls
 alias ll "ls -la"
 alias cat bat
+alias docker nerdctl
 alias libtool glibtool
 alias libtoolize glibtoolize
 alias e "code-insiders -a ."
-alias all-ruby "docker run --rm -it ghcr.io/ruby/all-ruby /all-ruby/all-ruby"
+alias all-ruby "nerdctl run --rm -it ghcr.io/ruby/all-ruby /all-ruby/all-ruby"
 alias s3cmd "s3cmd --config $HOME/.config/s3cmd"
 alias mvn "mvn -gs "$HOME"/.config/maven/settings.xml"
 alias wget "wget --hsts-file=$XDG_CACHE_HOME/wget-hsts"
