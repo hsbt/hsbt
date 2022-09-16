2a99c51fde925b128c95f723c2b5fdaa0715117b
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2022-09-16 13:34:38 +0900

    Added rubykaigi opml

---
+++ sandbox/rubykaigi-opml.rb
@@ -0,0 +1,35 @@
+require 'bundler/inline'
+
+gemfile do
+  source "https://rubygems.org"
+  gem "opml-parser"
+  gem "feedbag"
+  gem "rss"
+end
+
+require "open-uri"
+
+list = []
+
+1.upto(4) do |page|
+  url = "https://b.hatena.ne.jp/q/RubyKaigi?target=text&users=1&safe=off&date_begin=2022-09-01&mode=rss&page=#{page}"
+  xml = URI.open(url).read
+  rss = RSS::Parser.parse(xml)
+  rss.items.each do |item|
+    feeds = Feedbag.find(item.about)
+    next if feeds.empty?
+    next if feeds.first.include?("b.hatena.ne.jp")
+    list << feeds.first 
+  end
+end
+
+include OpmlParser
+
+outlines = list.flatten.uniq.map do |item|
+  OpmlParser::Outline.new({xmlUrl: item})
+end
+opml = OpmlParser.export(outlines, "RubyKaigi blogs")
+
+File.open("output.xml", "w") do |f|
+  f.write(opml)
+end
