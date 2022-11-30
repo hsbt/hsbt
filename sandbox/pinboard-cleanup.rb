9f61d4aaab0e0886e92b77509877d54470616176
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2022-11-30 17:48:21 +0900

    Added pinboard sample

---
+++ sandbox/pinboard-cleanup.rb
@@ -0,0 +1,19 @@
+require "bundler/inline"
+
+gemfile do
+  source "https://rubygems.org"
+  gem "pinboard", require: "pinboard"
+  gem "dotenv"
+end
+
+require "dotenv"
+Dotenv.load
+
+client = Pinboard::Client.new(token: ENV["PINBOARD_TOKEN"])
+
+# Get all bookmarks
+bookmarks = client.posts
+
+
+
+binding.irb
