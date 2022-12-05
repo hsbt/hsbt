a97e9920a77ec578d08c6ee69d8246f3589c5bd5
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2022-12-05 16:42:35 +0900

    Removed mvn configuration because it's not working under the ruby-maven

--- toolbox/xdg/config/fish/config.fish
+++ toolbox/xdg/config/fish/config.fish
@@ -8,7 +8,6 @@ alias libtoolize glibtoolize
 alias e "code-insiders -a ."
 alias all-ruby "nerdctl run --rm -it ghcr.io/ruby/all-ruby /all-ruby/all-ruby"
 alias s3cmd "s3cmd --config $HOME/.config/s3cmd"
-alias mvn "mvn -gs $HOME/.config/maven/settings.xml"
 alias wget "wget --hsts-file=$XDG_CACHE_HOME/wget-hsts"
 alias gpg "gpg --homedir "$XDG_DATA_HOME"/gnupg"
 alias yarn "yarn --use-yarnrc $XDG_CONFIG_HOME/yarn/config"
