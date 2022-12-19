850c262140d9a7e7ccfd55d926506d4ccfe97c18
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2022-12-19 16:30:51 +0900

    Added rename hsbt to fork

---
+++ sandbox/rename-git-hsbt-fork.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+require "parallel"
+
+IGNORE_REPOS = %w[
+]
+
+Dir.chdir(`ghq root`.chomp) do
+  Parallel.map(`ghq list`.split.shuffle) do |dir|
+    next if IGNORE_REPOS.any? { |repo| dir.include?(repo) }
+    next unless File.directory?(dir + "/.git")
+
+
+    Dir.chdir(dir) do
+      if `git remote -v | awk '{print $1}'`.match?(/hsbt/)
+        puts dir
+        system('git remote rename hsbt fork')
+      end
+    end
+  end
+end
