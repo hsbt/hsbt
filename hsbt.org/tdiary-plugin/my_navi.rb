532b1566b4f2e85c0ea6390f6948adbd99bdce27
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2022-12-27 07:05:44 +0900

    bundle exec standardrb --fix hsbt.org

--- hsbt.org/tdiary-plugin/my_navi.rb
+++ hsbt.org/tdiary-plugin/my_navi.rb
@@ -4,10 +4,10 @@
 # dropdown_calendar.rbを有効にしておくこと。
 # @options['dropdown_calendar.label']は空('')にしておく。
 def navi
-   result = %Q[<div class="adminmenu">\n]
-   result << calendar
-   result << navi_user
-   result << navi_admin
-   result << %Q[<a href="index.rdf"><img style="border-width: 0px;" src="https://www.hsbt.org/diary/images/feed-icon-12x12.png" width="12" height="12" alt="RSS feed"></a>]
-   result << %Q[</div>]
+  result = %(<div class="adminmenu">\n)
+  result << calendar
+  result << navi_user
+  result << navi_admin
+  result << %(<a href="index.rdf"><img style="border-width: 0px;" src="https://www.hsbt.org/diary/images/feed-icon-12x12.png" width="12" height="12" alt="RSS feed"></a>)
+  result << %(</div>)
 end
