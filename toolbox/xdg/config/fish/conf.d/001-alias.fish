1635c6d76ba2505607df253ad311ea832ecce75e
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2023-01-14 07:18:11 +0900

    use podman

--- toolbox/xdg/config/fish/conf.d/001-alias.fish
+++ toolbox/xdg/config/fish/conf.d/001-alias.fish
@@ -4,7 +4,7 @@ alias ll "ls -la"
 alias cat bat
 alias docker nerdctl
 alias e "code-insiders -a ."
-alias all-ruby "nerdctl run --rm -it ghcr.io/ruby/all-ruby /all-ruby/all-ruby"
+alias all-ruby "podman run --rm -it ghcr.io/ruby/all-ruby /all-ruby/all-ruby"
 alias s3cmd "s3cmd --config $HOME/.config/s3cmd"
 alias wget "wget --hsts-file=$XDG_CACHE_HOME/wget-hsts"
 alias gpg "gpg --homedir "$XDG_DATA_HOME"/gnupg"
