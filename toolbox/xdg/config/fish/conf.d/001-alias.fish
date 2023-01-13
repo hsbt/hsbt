be3f55a66d938c9f739c50d297c547507c815495
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2023-01-13 12:25:57 +0900

    restructured

---
+++ toolbox/xdg/config/fish/conf.d/001-alias.fish
@@ -0,0 +1,13 @@
+alias ls lsd
+alias l ls
+alias ll "ls -la"
+alias cat bat
+alias docker nerdctl
+alias libtool glibtool
+alias libtoolize glibtoolize
+alias e "code-insiders -a ."
+alias all-ruby "nerdctl run --rm -it ghcr.io/ruby/all-ruby /all-ruby/all-ruby"
+alias s3cmd "s3cmd --config $HOME/.config/s3cmd"
+alias wget "wget --hsts-file=$XDG_CACHE_HOME/wget-hsts"
+alias gpg "gpg --homedir "$XDG_DATA_HOME"/gnupg"
+alias yarn "yarn --use-yarnrc $XDG_CONFIG_HOME/yarn/config"
