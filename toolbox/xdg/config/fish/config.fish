ed8d36515882aeea19b2ad41fe2086459ad2be53
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2023-01-13 12:25:57 +0900

    restructured

--- toolbox/xdg/config/fish/config.fish
+++ toolbox/xdg/config/fish/config.fish
@@ -1,16 +1,7 @@
-alias ls lsd
-alias l ls
-alias ll "ls -la"
-alias cat bat
-alias docker nerdctl
-alias libtool glibtool
-alias libtoolize glibtoolize
-alias e "code-insiders -a ."
-alias all-ruby "nerdctl run --rm -it ghcr.io/ruby/all-ruby /all-ruby/all-ruby"
-alias s3cmd "s3cmd --config $HOME/.config/s3cmd"
-alias wget "wget --hsts-file=$XDG_CACHE_HOME/wget-hsts"
-alias gpg "gpg --homedir "$XDG_DATA_HOME"/gnupg"
-alias yarn "yarn --use-yarnrc $XDG_CONFIG_HOME/yarn/config"
+set -x XDG_CONFIG_HOME $HOME/.config
+set -x XDG_CACHE_HOME $HOME/.cache
+set -x XDG_DATA_HOME $HOME/.local/share
+set -x XDG_STATE_HOME $HOME/.local/state
 
 function fish_prompt
   set_color $fish_color_cwd
@@ -35,7 +26,6 @@ function history-merge --on-event fish_preexec
 end
 
 source /opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
-
-zoxide init fish --cmd j | source
-eval (direnv hook fish)
 source $HOME/.config/op/plugins.sh
+eval (direnv hook fish)
+zoxide init fish --cmd j | source
