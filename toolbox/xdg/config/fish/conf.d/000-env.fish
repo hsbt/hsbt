75c0308c01fad902902346e538fc5fae5e98fc3f
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2023-01-17 11:06:22 +0900

    Use the default macOS dir for XDG_CACHE_HOME

--- toolbox/xdg/config/fish/conf.d/000-env.fish
+++ toolbox/xdg/config/fish/conf.d/000-env.fish
@@ -1,7 +1,7 @@
 set -x XDG_CONFIG_HOME $HOME/.config
-set -x XDG_CACHE_HOME $HOME/.cache
 set -x XDG_DATA_HOME $HOME/.local/share
 set -x XDG_STATE_HOME $HOME/.local/state
+set -x XDG_CACHE_HOME $HOME/Library/Caches # macOS default
 
 set -x IRBRC $XDG_CONFIG_HOME/irb/irbrc
 set -x GEMRC $XDG_CONFIG_HOME/gem/gemrc
