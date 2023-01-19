595abb7a43c45e34cc8a6d99514bc4ed06dc5c17
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2023-01-19 20:49:17 +0900

    Remove Python 3.9

--- toolbox/xdg/config/fish/conf.d/000-env.fish
+++ toolbox/xdg/config/fish/conf.d/000-env.fish
@@ -40,7 +40,6 @@ set -x GO111MODULE on
 set -x RUBY_CODESIGN hsbt
 set -x RUBY_CONFIGURE_OPTS --disable-install-doc
 set -x PKG_CONFIG_PATH /opt/homebrew/opt/imagemagick@6/lib/pkgconfig:/usr/lib/pkgconfig
-set -x CLOUDSDK_PYTHON /opt/homebrew/opt/python@3.9/libexec/bin/python
 set -x GHQ_ROOT $HOME/Documents
 
 fish_add_path /opt/homebrew/opt/coreutils/libexec/gnubin
