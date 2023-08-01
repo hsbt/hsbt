b01e84b58100dc687e76c9bc35c60f65c234d4b1
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2023-08-01 19:04:41 +0900

    Added pubgrub samples

---
+++ sandbox/pubgrub/sample2.rb
@@ -0,0 +1,16 @@
+require "pub_grub"
+require "pub_grub/rubygems"
+
+source = PubGrub::StaticPackageSource.new do |s|
+  s.add 'foo', '2.0.0', deps: { 'bar' => '1.0.0' }
+  s.add 'foo', '1.0.0'
+  
+  s.add 'bar', '1.0.0', deps: { 'foo' => '1.0.0' }
+  
+  s.root deps: { 'bar' => '>= 1.0.0' }
+end
+
+solver = PubGrub::VersionSolver.new(source: source)
+result = solver.solve
+p result
+#=> {#<PubGrub::Package :root>=>0, "bar"=>#<Gem::Version "1.0.0">, "foo"=>#<Gem::Version "1.0.0">}
---
+++ sandbox/pubgrub/sample4.rb
@@ -0,0 +1,17 @@
+source = PubGrub::StaticPackageSource.new do |s|
+  s.add 'foo', '3.0.0', deps: { 'bar' => '> 1.0.0' }
+  s.add 'foo', '2.0.0', deps: { 'bar' => '1.0.0' }
+  s.add 'foo', '1.0.0'
+  
+  s.add 'bar', '1.0.0', deps: { 'foo' => '1.0.0' }
+  s.add 'bar', '2.0.0'
+
+  s.add 'buzz', '1.0.0', deps: { 'foo' => '> 1.0.0' }
+  
+  s.root deps: { 'buzz' => '1.0.0' }
+end
+
+solver = PubGrub::VersionSolver.new(source: source)
+result = solver.solve
+p result
+#=> {#<PubGrub::Package :root>=>0, "buzz"=>#<Gem::Version "1.0.0">, "foo"=>#<Gem::Version "3.0.0">, "bar"=>#<Gem::Version "2.0.0">}
