66acf618af094425e652f341c61d41e192004c98
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2024-01-10 12:16:55 +0900

    Don't alias ls and cat

--- toolbox/xdg/config/fish/conf.d/001-alias.fish
+++ toolbox/xdg/config/fish/conf.d/001-alias.fish
@@ -1,7 +1,5 @@
-alias ls lsd
-alias l ls
-alias ll "ls -la"
-alias cat bat
+alias l lsd
+alias ll "lsd -la"
 alias e "code-insiders -a ."
 alias all-ruby "podman run --rm -it ghcr.io/ruby/all-ruby /all-ruby/all-ruby"
 alias s3cmd "s3cmd --config $HOME/.config/s3cmd"
