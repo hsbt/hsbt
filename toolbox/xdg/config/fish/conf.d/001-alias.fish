1bb34098974b4427d5b915473a437fe9c639b836
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2024-01-31 17:16:06 +0900

    Use config.fish only

--- toolbox/xdg/config/fish/conf.d/001-alias.fish
+++
@@ -1,8 +0,0 @@
-alias l lsd
-alias ll "lsd -la"
-alias e "code-insiders -a ."
-alias all-ruby "podman run --rm -it ghcr.io/ruby/all-ruby /all-ruby/all-ruby"
-alias s3cmd "s3cmd --config $HOME/.config/s3cmd"
-alias wget "wget --hsts-file=$XDG_CACHE_HOME/wget-hsts"
-alias gpg "gpg --homedir "$XDG_DATA_HOME"/gnupg"
-alias yarn "yarn --use-yarnrc $XDG_CONFIG_HOME/yarn/config"
