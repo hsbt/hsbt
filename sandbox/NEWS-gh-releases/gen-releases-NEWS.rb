210591d92c87e655a3cdae5db6d1949ffddd975b
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2024-12-17 18:05:18 +0900

    init gh-releases references generater

---
+++ sandbox/NEWS-gh-releases/gen-releases-NEWS.rb
@@ -0,0 +1,56 @@
+#!/usr/bin/env ruby
+
+require "bundler/inline"
+
+gemfile do
+  source "https://rubygems.org"
+  gem "octokit"
+  gem "faraday-retry"
+end
+
+Octokit.configure do |c|
+  c.access_token = ENV['GITHUB_TOKEN']
+  c.auto_paginate = true
+  c.per_page = 100
+end
+
+versions_from = File.readlines(ARGV[0]).map(&:split).to_h
+versions_to = File.readlines(ARGV[1]).map(&:split).to_h
+footnote_link = []
+
+versions_to.each do |name, version|
+  releases = []
+  org = "ruby"
+
+  case name
+  when "RubyGems"
+    name = name.downcase
+    org = "rubygems"
+  when "bundler"
+    name = "rubygems"
+    org = "rubygems"
+  when "minitest"
+    org = "minitest"
+  when "test-unit"
+    org = "test-unit"
+  end
+  
+  Octokit.releases("#{org}/#{name}").each do |release|
+    releases << release.tag_name
+  end
+  releases.reverse!
+
+  start_index = releases.index("v#{versions_from[name]}")
+  end_index = releases.index("v#{versions_to[name]}")
+  release_range = releases[start_index..end_index] if start_index && end_index
+
+  next unless release_range
+
+  puts "* #{name} #{version}"
+  puts "  * #{release_range.map{|rel| "[#{rel}][#{name}-#{rel}]"}.join(", ")}"
+  release_range.each do |rel|
+    footnote_link << "[#{name}-#{rel}]: https://github.com/#{org}/#{name}/releases/tag/#{rel}"
+  end
+end
+
+puts footnote_link.join("\n")
