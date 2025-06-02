df735a0c200386c06e387787536e09ff14ab7bc5
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2025-06-02 11:16:54 +0900

    Extract new gem

--- labs/gem-clone/lib/rubygems_plugin.rb
+++
@@ -1,4 +0,0 @@
-require 'rubygems/command_manager'
-require 'rubygems/commands/clone_command'
-
-Gem::CommandManager.instance.register_command :clone
--- labs/gem-clone/test/test_helper.rb
+++
@@ -1,4 +0,0 @@
-$LOAD_PATH.unshift File.expand_path('../lib', __dir__)
-
-require 'minitest/autorun'
-require 'rubygems/commands/clone_command'
