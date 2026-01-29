6d929daaa16521e9e4ff7684da73e3d2e12f979b
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2026-01-29 12:16:35 +0900

    Refer the latest version of highlight.js

--- hsbt.org/tdiary-plugin/highlight-js.rb
+++ hsbt.org/tdiary-plugin/highlight-js.rb
@@ -1,7 +1,7 @@
 add_header_proc do
   <<~STYLE
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/default.min.css">
-    <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js"></script>
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/latest/styles/default.min.css">
+    <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/latest/highlight.min.js"></script>
     <script>hljs.initHighlightingOnLoad();</script>
   STYLE
 end
